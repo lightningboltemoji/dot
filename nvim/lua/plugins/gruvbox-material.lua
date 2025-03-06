b51da883b0bc528d0a6cd44b1f59d0013b08dcd4
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2025-03-05 22:28:07 -0800

    nvim updates, color scheme

---
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_transparent_background = true
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
