a14da55a835a00711273b1ecc8edbaca289e936f
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2025-09-15 22:53:52 -0700

    misc updates

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "q", "<Nop>")
+vim.keymap.set("n", "<C-`>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end)
+vim.keymap.set("t", "<C-`>", "<cmd>close<cr>", { desc = "Hide Terminal" })
